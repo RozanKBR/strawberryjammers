0b134ed92b211f01e528615ea6d1c0880e3e66d9
RobinWatson <robwatson44@hotmail.co.uk>
2016-04-16 12:40:33 +0100

    Weapon and bsic gun script

---
+++ TeamProject/Assets/Scripts/Weapons/Gun.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : Weapon
+{
+   
+
+	// Use this for initialization
+	
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextAttack)
+        {
+            attackMove(Vector3.forward);
+        }
+	}
+}
---
+++ TeamProject/Assets/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Weapon : MonoBehaviour
+{
+    protected float coolDown = 0.2f;
+    protected float nextAttack = 0.0f;
+
+    protected float damage = 5.0f;
+    //protected Collider2D b_Collider;
+
+    protected new Transform transform;
+    protected GameObject attackShot;
+    // Use this for initialization
+
+    void Awake()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+    public void attackMove(Vector3 direction)
+    {              
+            nextAttack = Time.time + coolDown;
+            GameObject clone = Instantiate(attackShot, transform.position, transform.rotation) as GameObject;      
+    }
+}
