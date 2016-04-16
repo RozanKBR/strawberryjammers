c729c7292b1e83b86778bb8380f43970b2a6f16e
Vitor <rozankbr@outlook.com>
2016-04-16 11:36:06 +0100

    Inital Commit

---
+++ TeamProject/Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿#region Using Statements
+using UnityEngine;
+using System.Collections;
+#endregion
+
+
+public class PlayerController : MonoBehaviour
+{
+    private new Transform transform;
+    private new SpriteRenderer renderer;
+    private Sprite m_sprite;
+
+    [Header ("Character Movement")]
+    [SerializeField] private float m_characterSpeed = 5f;
+    [SerializeField] private float m_rollingDuration = 1f;
+    [SerializeField] [Range (0, 100)] private int m_rollingSpeedModifier = 20;
+
+    private bool m_is_rolling = false;
+    private float m_current_roll_duration = 0f;
+
+    void Awake()
+    {
+        transform = GetComponent<Transform>();
+        renderer = GetComponent<SpriteRenderer>();
+
+        m_sprite = renderer.sprite;
+    }
+
+    void Update()
+    {
+        if (GameManager._Instance.MGameState == GameState.Paused)
+            return;
+
+        // the game is running excute the code
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.position +=   (Vector3.right * h * m_characterSpeed) + 
+                                (Vector3.forward * v * m_characterSpeed);
+    }
+
+    private void PlayerRolling()
+    {
+        if (m_is_rolling)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+}
---
+++ TeamProject/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,49 @@
+﻿#region Using Statements
+using UnityEngine;
+using System.Collections;
+#endregion
+
+public enum GameState
+{
+    Running = 0x1,
+    Paused = 0x2
+}
+
+
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton
+    private static GameManager _instance;
+    public static GameManager _Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = FindObjectOfType<GameManager>();
+
+            if (_instance == null)
+                Debug.LogError("There is no Game Manager on the Scene");
+
+            return _instance;
+        }
+    }
+    #endregion
+
+
+    public GameState MGameState { private set; get; }
+
+
+    void Awake()
+    {
+        if (_instance != null)
+            Destroy(this);
+
+        _instance = this;
+    }
+
+    void Start()
+    {
+
+    }
+}
